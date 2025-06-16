70303bb84b4d0fc44b93dfe948d551030ae45034
MagoCode30 <lapm1102@gmail.com>
2025-06-16 16:18:13 +0100

    Complete ServicosActivity

--- app/src/main/java/com/example/cesaeapp/servicos/ServicosActivity.kt
+++ app/src/main/java/com/example/cesaeapp/servicos/ServicosActivity.kt
@@ -6,16 +6,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.cesaeapp.R
+import com.example.cesaeapp.databinding.ActivityServicosBinding
 
 class ServicosActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityServicosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityServicosBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_servicos)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Serviços"
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
